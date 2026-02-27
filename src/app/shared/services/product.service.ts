31adcb206c7852a567d8d58ab65663edbf7da015
mdoman765 <mdoman765@gmail.com>
2026-02-27 14:41:53 +0600

    fix(product-form): fix errors in template and service
    Resolved errors in Productuformcomponent.html and product.service.ts.

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { Product, CreateProduct, UpdateProduct } from '../models/product.model';
+import { Product, CreateProduct, UpdateProduct } from '../../core/models/product.model';
 
 
 
